9ca407fac923320c12880801a22209880e17cfc1
can97 <can97@DESKTOP-ETFFE43>
2020-11-29 20:52:03 +0000

    Kata3V2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,8 +3,12 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-           HistogramDisplay histo = new HistogramDisplay();
-           histo.execute();
+           Histogram<String> histo = new Histogram<>();
+           histo.increment("gmail.com");
+           histo.increment("gmail.com");
+           histo.increment("gmail.com");
+           histo.increment("gmail.es");
+           new HistogramDisplay(histo).execute();
     }
     
 }
